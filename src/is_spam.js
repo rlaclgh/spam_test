e8aed86e4a4eeaed18f983695f210a378056c4e1
rlaclgh <rlaclgh11@gmail.com>
2024-03-14 15:37:27 +0900

    ADD is_spam

--- src/is_spam.js
+++ src/is_spam.js
@@ -1,5 +1,67 @@
-const isSpam = () => {
+const extractUrls = require("./extract_urls");
+const getRedirectDomain = require("./get_redirect_domain");
+const getRedirectDomainByContent = require("./get_redirect_domains_by_content");
+const extractDomain = require("./extract_domain");
+
+const isSpam = async (content, spamLinkDomains, redirectionDepth) => {
+  const checked = {};
+  const urls = extractUrls(content);
+  if (urls.length === 0) return true;
+  if (spamLinkDomains.length === 0) return true;
+
+  for (const url of urls) {
+    const result = await isSpamRecursive(
+      0,
+      url,
+      spamLinkDomains,
+      redirectionDepth,
+      checked
+    );
+    if (result) {
+      return true;
+    }
+  }
+
   return false;
+};
+
+const isSpamRecursive = async (
+  depth,
+  url,
+  spamLinkDomains,
+  redirectionDepth,
+  checked
+) => {
+  if (depth > redirectionDepth) {
+    return false;
+  }
+
+  let allUrls = [];
+
+  if (url in checked) {
+    allUrls = checked[url];
+  } else {
+    const redirectDomain = await getRedirectDomain(url);
+    const domainSet = await getRedirectDomainByContent(url);
+    domainSet.add(redirectDomain);
+    allUrls = Array.from(domainSet);
+  }
+
+  for (const domain of allUrls) {
+    if (spamLinkDomains.includes(extractDomain(domain))) {
+      return true;
+    }
+  }
+
+  for (const domain of domainSet) {
+    isSpamRecursive(
+      depth + 1,
+      domain,
+      spamLinkDomains,
+      redirectionDepth,
+      checked
+    );
+  }
 };
 
 module.exports = isSpam;
--- test/is_spam.spec.js
+++ test/is_spam.spec.js
@@ -1,4 +1,16 @@
 const isSpam = require("../src/is_spam");
-test("init test", () => {
-  expect(isSpam()).toBe(false);
+test("init test", async () => {
+  const result1 = await isSpam(
+    "spam spam https://moiming.page.link/exam?_imcp=1",
+    ["docs.github.com"],
+    1
+  );
+
+  const result2 = await isSpam(
+    "spam spam https://moiming.page.link/exam?_imcp=1",
+    ["moiming.page.link"],
+    1
+  );
+  expect(result1).toBe(false);
+  expect(result2).toBe(true);
 });
